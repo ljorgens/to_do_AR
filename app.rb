36a4515001286b9ed61c7cf2f79ff30555506503
Lee Jorgensen & David Zulaica <student@epicodus.com>
2015-01-26 10:31:31 -0800

    Add rake stuff.

--- app.rb
+++ app.rb
@@ -1,10 +1,9 @@
 require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
+require("sinatra/activerecord")
 require("./lib/task")
 require("pg")
-
-DB = PG.connect({:dbname => 'to_do'})
 
 get('/') do
   @tasks = Task.all()
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,6 @@ require("rspec")
 require("pg")
 require("task")
 
-DB = PG.connect({:dbname => 'to_do_test'})
-
 RSpec.configure do |config|
   config.after(:each) do
     DB.exec("DELETE FROM tasks *;")
